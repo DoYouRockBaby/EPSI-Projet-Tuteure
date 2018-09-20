805c7946d168554295a2f383d116760f2178db5f
Brahim IDRYSY <brahim.idrysy@gmail.com>
2018-09-20 13:01:01 +0200

    creation du vehicleprovider

---
+++ Carsale.DAO/Providers/VechicleProvider.cs
@@ -0,0 +1,55 @@
+﻿using Carsale.DAO.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carsale.DAO.Providers
+{
+   public class VechicleProvider
+    {
+        public void Add(Vehicle vehicle)
+        {
+            using (var carsaleContext = new CarsaleContext())
+            {
+                carsaleContext.Vehicles.Add(vehicle);
+                carsaleContext.SaveChanges();
+            }
+        }
+        public IEnumerable<Vehicle> FindAll()
+        {
+            using (var context = new CarsaleContext())
+            {
+                return context.Vehicles.ToList();
+            }
+        }
+        public Vehicle FindByMatriculation(string matriculation)
+        {
+            using (var context = new CarsaleContext())
+            {
+                return context.Vehicles.Where(e => e.Matriculation == matriculation).FirstOrDefault();
+            }
+        }
+        public void Update(Vehicle vehicle)
+        {
+            using (var context = new CarsaleContext())
+            {
+                context.Entry(vehicle).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(String matriculation)
+        {
+            using (var context = new CarsaleContext())
+            {
+                context.Entry(FindByMatriculation(matriculation)).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
