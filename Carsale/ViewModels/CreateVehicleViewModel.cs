540f283d705d8e091baaec8cc8bec4f62e2c7a6b
Brahim IDRYSY <brahim.idrysy@gmail.com>
2018-09-20 15:13:41 +0200

    Temp

---
+++ Carsale/ViewModels/CreateVehicleViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Carsale.DAO.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Carsale.ViewModels
+{
+    public class CreateVehicleViewModel
+    {
+
+
+        [Required]
+        public Vehicle Vehicle { get; set; }
+
+        [Required]
+        public IEnumerable<Status> Status
+        {
+            get
+            {
+                return (Status[])Enum.GetValues(typeof(Status));
+            }
+        }
+
+        [Required]
+        public IEnumerable<VehicleColor> VehicleColor
+        {
+            get
+            {
+                return (VehicleColor[])Enum.GetValues(typeof(VehicleColor));
+            }
+        }
+    }
+}
