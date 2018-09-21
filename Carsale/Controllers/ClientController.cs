e55d236eccb365ea194fe37952857407a211269c
ssheikhi <32467572+ssheikhi@users.noreply.github.com>
2018-09-21 09:45:53 +0200

    add client actionresult

--- Carsale/Controllers/ClientController.cs
+++ Carsale/Controllers/ClientController.cs
@@ -1,14 +1,17 @@
-﻿using Carsale.DAO.Models;
+﻿using Carsale.DAO;
+using Carsale.DAO.Models;
 using Carsale.DAO.Providers;
 using English_Battle_MVC.Attributes;
 using System.Web.Mvc;
 
 namespace Carsale.Controllers
 {
-    [LoggedAuthorization(AllowedTypes = new AccountType[] { AccountType.Director, AccountType.NewVehicleTrader, AccountType.OldVehicleTrader, AccountType.AccountingDepartmentManager })]
+  //  [LoggedAuthorization(AllowedTypes = new AccountType[] { AccountType.Director, AccountType.NewVehicleTrader, AccountType.OldVehicleTrader, AccountType.AccountingDepartmentManager })]
     public class ClientController : Controller
     {
         ClientProvider clientProvider;
+        CarsaleContext db = new CarsaleContext();
+
 
         public ClientController(ClientProvider clientProvider)
         {
@@ -27,5 +30,63 @@ namespace Carsale.Controllers
             ViewBag.Clients = clientProvider.FindAll();
             return View();
         }
+
+        public ActionResult AddClient()
+        {
+            return View();
+        }
+        //Créer une action ajouter un client dans le controlleur
+        [HttpPost]
+        public ActionResult AddClient(Client client)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.Clients.Add
+                    (new DAO.Models.Client()
+                    {
+
+                        FirstName = client.FirstName,
+                        LastName = client.LastName,
+                        SIRET = client.SIRET,
+                        Name = client.Name,
+                        Description = client.Description,
+                        Address1 = client.Address1,
+                        Address2 = client.Address2,
+                        ZipCode = client.ZipCode,
+                        Country = client.Country
+
+                    });
+                db.SaveChanges();
+                ViewBag.Error = "Data Added Success!";
+                return View();
+            }
+            ViewBag.Error = "Error Data Is Not Match!";
+            return View();
+        }
+        ////If the post informations are valid, insert the user in the database
+        //    if (ModelState.IsValid)
+        //    {
+        //        var errorOccured = false;
+        //        if (viewModel.RepeatPassword != viewModel.Account.Password)
+        //        {
+        //            ViewBag.RepeatPasswordError = "Les mots de passes ne correspondent pas.";
+        //            errorOccured = true;
+        //        }
+
+        //        if (accountProvider.FindByEmail(viewModel.Account.Email) != null)
+        //        {
+        //            ViewBag.EmailError = "L'email renseigné existe déjà dans la base de donnée";
+        //            errorOccured = true;
+        //        }
+
+        //        if (!errorOccured)
+        //        {
+        //            accountProvider.Add(viewModel.Account);
+        //            return RedirectToAction("List");
+        //        }
+        //    }
+
+        //    return View(viewModel);
     }
 }
