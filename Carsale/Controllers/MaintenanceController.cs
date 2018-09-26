ac2e734661e2529b3b1f1844dcd931f7890b2ea6
Léo Le Sommer <llesommer.dev@gmail.com>
2018-09-26 09:33:07 +0200

    Creation du modele entretient

---
+++ Carsale/Controllers/MaintenanceController.cs
@@ -0,0 +1,33 @@
+﻿using Carsale.ViewModels;
+using System.Web.Mvc;
+
+namespace Carsale.Controllers
+{
+    public class MaintenanceController : Controller
+    {
+        // GET: Maintenance/List
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        // GET: Maintenance/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Maintenance/Create
+        [HttpPost]
+        public ActionResult Create(CreateMaintenanceViewModel viewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                // TODO: Add insert logic here
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Carsale/ViewModels/CreateMaintenanceViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Carsale.DAO.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Carsale.ViewModels
+{
+    public class CreateMaintenanceViewModel
+    {
+        [Required]
+        public Maintenance Maintenance { get; set; }
+
+        public IEnumerable<Part> Parts { get; set; }
+    }
+}
