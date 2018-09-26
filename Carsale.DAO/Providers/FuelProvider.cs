17aae569e92a317a6ae5a12efbd342881bbec560
Léo Le Sommer <llesommer.dev@gmail.com>
2018-09-26 15:56:25 +0200

    Ajout des provider pour les carburants

---
+++ Carsale.DAO/Providers/FuelProvider.cs
@@ -0,0 +1,53 @@
+﻿using Carsale.DAO.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Carsale.DAO.Providers
+{
+    public class FuelProvider
+    {
+        public void Add(Fuel fuel)
+        {
+            using (var context = new CarsaleContext())
+            {
+                context.Fuels.Add(fuel);
+                context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Fuel> FindAll()
+        {
+            using (var context = new CarsaleContext())
+            {
+                return context.Fuels.ToList();
+            }
+        }
+
+        public Fuel FindById(int id)
+        {
+            using (var context = new CarsaleContext())
+            {
+                return context.Fuels.Find(id);
+            }
+        }
+
+        public void Update(Fuel fuel)
+        {
+            using (var context = new CarsaleContext())
+            {
+                context.Entry(fuel).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var context = new CarsaleContext())
+            {
+                context.Entry(FindById(id)).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Carsale.DAO/Providers/FuelSaleProvider.cs
@@ -0,0 +1,53 @@
+﻿using Carsale.DAO.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Carsale.DAO.Providers
+{
+    public class FuelSaleProvider
+    {
+        public void Add(FuelSale fuelSales)
+        {
+            using (var context = new CarsaleContext())
+            {
+                context.FuelSales.Add(fuelSales);
+                context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<FuelSale> FindAll()
+        {
+            using (var context = new CarsaleContext())
+            {
+                return context.FuelSales.ToList();
+            }
+        }
+
+        public FuelSale FindById(int id)
+        {
+            using (var context = new CarsaleContext())
+            {
+                return context.FuelSales.Find(id);
+            }
+        }
+
+        public void Update(FuelSale fuelSales)
+        {
+            using (var context = new CarsaleContext())
+            {
+                context.Entry(fuelSales).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var context = new CarsaleContext())
+            {
+                context.Entry(FindById(id)).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
